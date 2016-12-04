bc588cd0d58c334debea0dc0d205ab74523524ef
winga45 <winga45@gmail.com>
2016-12-04 16:17:50 -0500

    Revert "wacko"
    This reverts commit c053f272dfdf82a9f38ec8cb00d688000ab9580f.

--- core/product_config.mk
+++ core/product_config.mk
@@ -179,22 +179,21 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# Bolt-Os build needs only the TO product makefiles.
-ifneq ($(BOLT_BUILD),)
-  all_product_configs := $(shell ls vendor/bolt/products/dk_$(BOLT_BUILD).mk)
+ifneq ($(strip $(TARGET_BUILD_APPS)),)
+# An unbundled app build needs only the core product makefiles.
+all_product_configs := $(call get-product-makefiles,\
+    $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-  ifneq ($(strip $(TARGET_BUILD_APPS)),)
-  # An unbundled app build needs only the core product makefiles.
-  all_product_configs := $(call get-product-makefiles,\
-      $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
+  ifneq ($(CUSTOM_BUILD),)
+    all_product_configs := $(shell ls vendor/benzo/products/benzo_$(CUSTOM_BUILD).mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif # TARGET_BUILD_APPS
-endif # BOLT_BUILD
+  endif
+endif
 
-ifeq ($(BOLT_BUILD),)
+ifeq ($(CUSTOM_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
